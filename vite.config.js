fec12c594bb61e0fdf6803b4ef110e89237bf0cb
arnaldotorr <arnaldo.torrealba@gmail.com>
2023-10-10 15:56:50 -0300

    fix base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/DesafioLatam-18/',
-  build: {
-    outDir: './docs'
-  }
-})
+    plugins: [react()],
+    base: "/DesafioLatam-19/",
+    build: {
+        outDir: "./docs",
+    },
+});
